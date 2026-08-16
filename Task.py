2e383dceea1b1a2dee686a171db59e581eecf919
krishnatejarevalla <krishnatejarevalla@gmail.com>
2026-08-16 21:11:40 +0530

    Updated cellvalues in Excel Sheet using python

--- Task.py
+++ Task.py
@@ -1,8 +1,23 @@
 #Automating Excel using openpyxl module
+
+#Accessing Worksheets from workbooks and cellvalues present in it
 from openpyxl import Workbook, load_workbook
 wb = load_workbook("Marks.xlsx")
 ws = wb.active
 print(ws['A2'].value) #Arjun
+print(ws['A3'].value) #Priya
 print(ws['B6'].value) #69
 print(ws.max_row) #9 rows
 print(ws.max_column) #5 cols
+
+#changing the cellvalues
+ws['A2'] = "Teja"
+ws['A3'] = "Srinu"
+ws['B6'] = 79
+
+#saving the changes into Excel file
+wb.save('Marks.xlsx')
+
+
+#Accessing the sheet names
+print(wb.sheetnames) #Grades
