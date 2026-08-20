85d2575858c1243c9e40f4b71142705d57db0710
krishnatejarevalla <krishnatejarevalla@gmail.com>
2026-08-20 19:01:11 +0530

    Using column_index_from_string

--- Practice_Utils.py
+++ Practice_Utils.py
@@ -23,3 +23,16 @@ for col in range(1, ws.max_column+1):
             # D= SQL
             # E= Excel
             # F= PowerBI
+
+#Using column_index_from_string
+from openpyxl.utils import column_index_from_string
+for letter in range(65, 65 + ws.max_column):
+    letter = chr(letter)
+    print(letter, "=", column_index_from_string(letter))
+                # OUTPUT
+                # A = 1
+                # B = 2
+                # C = 3
+                # D = 4
+                # E = 5
+                # F = 6 
