7d29f00812e2c5291eb35c359407c5cf11fc6863
krishnatejarevalla <krishnatejarevalla@gmail.com>
2026-08-20 18:28:29 +0530

    Practicing basic column operation

---
+++ Practice_Utils.py
@@ -0,0 +1,14 @@
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
+wb = load_workbook("MarkList.xlsx")
+ws = wb.active
+
+#printing column names using get_column_letter
+for col in range(1,7):
+    print("Column "+ str(col) , "=" ,get_column_letter(col))
+            #Output:  Column 1 = A
+                    # Column 2 = B
+                    # Column 3 = C
+                    # Column 4 = D
+                    # Column 5 = E
+                    # Column 6 = F
