d27b54db22c918828155091cadc62f8972cf661c
krishnatejarevalla <krishnatejarevalla@gmail.com>
2026-08-24 13:57:05 +0530

    Added Search function using Keyword to find cell address

--- L4_Practice.py
+++ L4_Practice.py
@@ -1,5 +1,5 @@
 from openpyxl import load_workbook
-from openpyxl.utils import range_boundaries
+from openpyxl.utils import range_boundaries, column_index_from_string
 wb = load_workbook("MarkList.xlsx")
 ws = wb.active
 
@@ -7,17 +7,17 @@ ws = wb.active
 cell_range = input("Enter the range of cells: ")
 min_col, min_row, max_col, max_row = range_boundaries(cell_range)
 print(f"Starting row : {min_row}",
-      f"Starting col : {min_col}",
-      f"ending row :{max_row}",
-      f"ending col : {max_col}"
-      , end =",")
+       f"Starting col : {min_col}",
+       f"ending row :{max_row}",
+       f"ending col : {max_col}"
+       , end =",")
         #Output:
         #Enter the range of cells: A1:F11
         #Starting row : 1 Starting col : 1 ending row :11 ending col : 6,
 
 #printing cell values from given range
 for row in range(min_row, max_row+1):
-    for col in range(min_col, max_col+1):
+     for col in range(min_col, max_col+1):
         print(ws.cell(row = row, column= col).value, end = " ")
     print()
                 # Output:
@@ -33,3 +33,26 @@ for row in range(min_row, max_row+1):
                 # 108 Divya 89 93 88 90 
                 # 109 Vijay 70 68 75 72 
                 # 110 Neha 86 90 92 87 
+
+
+#Printing values from specific column
+Column_letter = input("Enter column letter: ")
+col_num = column_index_from_string(Column_letter)
+for row in range(min_row, max_row+1):
+    print(ws.cell(row=row, column= col_num).value)
+
+#Searching for a value and it's cell coordinates
+Keyword = input("Enter the value to search : ")
+found = False
+for row in range(min_row, max_row+1):
+    for col in range(min_col, max_col+1):
+        if ws.cell(row=row, column = col).value == Keyword:
+            print(f"{Keyword} is found at" ,ws.cell(row, col).coordinate)
+            found =True
+if found is not True:
+    print("Not Found")
+            #output:
+            # Enter the range of cells: A1:F11
+            # Starting row : 1 Starting col : 1 ending row :11 ending col : 6,Enter the value to search : Priya
+            # Priya is found at B3
+
